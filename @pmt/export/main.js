2786098abf81db7adcaa39a1dc86f9fcf46c42ca
STEN <sten.li@foxmail.com>
2026-03-03 11:26:43 +0800

    fix relative path issue inside napi api

--- @pmt/export/main.js
+++ @pmt/export/main.js
@@ -1,7 +1,50 @@
 import os from 'node:os';
 import child_process from 'node:child_process';
+import { fileURLToPath } from 'node:url';
+import { dirname, resolve } from 'node:path';
 
-import * as api from './api/index.js';
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+// api loader to ensure the cwd is correct when running api function in case it relies on relative path
+function createApiLoader(apiDir) {
+  const apiLoader = {
+    async load() {
+      if (this.api) return this.api;
+      const cwd = process.cwd();
+      process.chdir(apiDir);
+      try {
+        this.api = await import('./api/index.js');
+      } finally {
+        process.chdir(cwd);
+      }
+      return this.api;
+    },
+    run(apiFn) {
+      return function (...args) {
+        const cwd = process.cwd();
+        process.chdir(apiDir);
+        let res;
+        try {
+          res = apiFn(...args);
+        } catch (err) {
+          process.chdir(cwd);
+          throw err;
+        }
+        if (typeof res?.then === 'function') {
+          return res.finally(() => process.chdir(cwd));
+        }
+        process.chdir(cwd);
+        return res;
+      };
+    },
+    api: null,
+  };
+  return apiLoader;
+}
+const apiDir = resolve(__dirname, 'api');
+const apiLoader = createApiLoader(apiDir);
+const apiReady = apiLoader.load();
 
 const platform = process.platform || os.platform();
 const isMac = platform === 'darwin';
@@ -70,8 +113,8 @@ export default {
   api: {
     test: (...args) => ({ args }),
 
-    exportParsedStandardDirectory: api.exportParsedStandardDirectory,
-    // deidentify2DDicom: api.deidentify2DDicom,
+    exportParsedStandardDirectory: async (...args) => apiReady.then(api => apiLoader.run(api.exportParsedStandardDirectory)(...args)),
+    // deidentify2DDicom: async (...args) => apiReady.then(api => apiLoader.run(api.deidentify2DDicom)(...args)),
 
     // ...
 
